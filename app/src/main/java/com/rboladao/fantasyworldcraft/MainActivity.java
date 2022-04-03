eae5284def232be0fc31746901dd01c8e195014b
Rafael-Dev-21 <victorrafaelpereiraalves0202@gmail.com>
2022-04-02 22:54:41 -0300

    Initial project

---
+++ app/src/main/java/com/rboladao/fantasyworldcraft/MainActivity.java
@@ -0,0 +1,17 @@
+package com.rboladao.fantasyworldcraft;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.rboladao.fantasyworldcraft.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
